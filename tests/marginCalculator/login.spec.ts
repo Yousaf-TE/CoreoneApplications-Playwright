a4d287cfa965bb2539d9e37aeeadf8d74c881680
YousafMurad-TE <yousaf.murad@testequity.com>
2025-12-16 18:19:51 +0500

    fixes - automation

--- tests/marginCalculator/login.spec.ts
+++ tests/marginCalculator/login.spec.ts
@@ -6,8 +6,13 @@ import { DashboardPage } from '../../pages/dashboard.page';
 
 test('Login to Margin Calculator (POM)', async () => {
 
-  const email = process.env.TEQ_EMAIL;      
+  const email = process.env.TEQ_EMAIL;
   const password = process.env.TEQ_PASSWORD;
+
+  if (!email || !password) {
+    throw new Error('Environment variables TEQ_EMAIL or TEQ_PASSWORD are missing');
+  }
+
   const context = await createContext();
   const page = await context.newPage();
 
@@ -18,8 +23,8 @@ test('Login to Margin Calculator (POM)', async () => {
   await login.navigate();
   await login.openLoginWindow();
 
-  await login.enterEmail(email!);
-  await login.enterPassword(password!);
+  await login.enterEmail(email);
+  await login.enterPassword(password);
   await mfa.continueAfterPassword();
 
   await mfa.skipSecuritySetup();
